87fd39daa93117662f8c416bf00a18351319aeb3
Vini Bidin <vinibidin@gmail.com>
2021-02-15 22:21:59 -0300

    refatora

--- binToDec/script.js
+++ binToDec/script.js
@@ -1,25 +1,32 @@
 const binary = document.querySelector("#binary");
 const decimal = document.querySelector("#decimal");
-let value = 0;
+const key = {
+  ENTER: 13,
+  ZERO: 48,
+  ONE: 49,
+  BACKSPACE: 8,
+  CTRL: 17
+};
+const allowedKeys = [key.ENTER, key.ZERO, key.ONE, key.BACKSPACE, key.CTRL];
 
+binary.addEventListener("keydown", function(event) {
+  const pressedKey = event.keyCode;
+  let sum;
 
-
-binary.addEventListener('keyup', function (event) {
-  if (event.keyCode === 13) {
-    value = binary.value;
-    valueArray = value.split("");
-    len = valueArray.length;
+  if (pressedKey === key.ENTER) {
+    const value = binary.value;
+    const valueArray = value.split("");
+    const len = valueArray.length;
     sum = 0;
-    for (var i = 0; len > i; i++) {
-      var powers = valueArray[i] * (2 ** ((len - 1) - i));
+
+    for (let i = 0; len > i; i++) {
+      const powers = valueArray[i] * (2 ** ((len - 1) - i));
       sum += powers;
-
     }
-    console.log(sum);
-  } else if ((event.keyCode !== 48) && (event.keyCode !== 49) && (event.keyCode !== 8) && (event.keyCode !== 17)) {
-    window.alert("Only 0's and 1's bruh")
-    binary.value = "";
-  }
-  decimal.textContent = sum
 
-})
+  } else if (!allowedKeys.includes(pressedKey)) {
+    event.preventDefault();
+    window.alert("Only 0's and 1's bruh")
+  }
+  decimal.textContent = sum;
+});
