a78066a435dc76d69cf153facde1ab1e1f4c0e93
Vini Bidin <vinibidin@gmail.com>
2021-02-18 01:24:18 -0300

    refatora e implementa nova feature

--- comparator/script.js
+++ comparator/script.js
@@ -1,50 +1,65 @@
 const number1 = document.getElementById("num-input1");
 const number2 = document.getElementById("num-input2");
-const selector = document.getElementById("num-selector");
-const button = document.getElementById("num-button");
-const output = document.querySelector("#num-output span")
+const txt1 = document.getElementById("txt-input1");
+const txt2 = document.getElementById("txt-input2");
+const txtSelector = document.getElementById("txt-selector")
+const numSelector = document.getElementById("num-selector");
+const numButton = document.getElementById("num-button");
+const txtButton = document.getElementById("txt-button");
+const numOutput = document.querySelector("#num-output span");
+const txtOutput = document.querySelector("#txt-output span");
 
 let newNum1 = 0;
 let newNum2 = 0;
 number1.value = 0;
 number2.value = 0;
 
+let newTxt1, newTxt2;
+txt1.value = "";
+txt2.value = "";
+
 number1.addEventListener("change", function() {
-  newNum1 = parseInt(this.value);
-  this.value = parseInt(this.value);
-  console.log(newNum1);
+  newNum1 = this.value;
 });
 
 number2.addEventListener("change", function() {
-  newNum2 = parseInt(this.value);
-  this.value = parseInt(this.value);
-  console.log(newNum2);
+  newNum2 = this.value;
 });
 
-button.addEventListener("click", function() {
-  if (selector.value === "equals-to") {
-    if (newNum1 === newNum2) {
-      output.textContent = "True";
-    } else {
-      output.textContent = "False";
-    };
-  }else if (selector.value === "less-than") {
-    if (newNum1 < newNum2) {
-      output.textContent = "True";
-    } else {
-      output.textContent = "False";
-    };
-  }else if (selector.value === "greater-than") {
-    if (newNum1 > newNum2) {
-      output.textContent = "True";
-    } else {
-      output.textContent = "False";
-    };
-  }else if (selector.value === "other-than") {
-    if (newNum1 !== newNum2) {
-      output.textContent = "True";
-    } else {
-      output.textContent = "False";
-    };
-  }
+numButton.addEventListener("click", function() {
+  newNum1 = parseInt(newNum1);
+  newNum2 = parseInt(newNum2);
+
+  if (numSelector.value === "equals-to") {
+    numOutput.textContent = (newNum1 === newNum2) ? "True" : "False";
+
+  }else if (numSelector.value === "less-than") {
+    numOutput.textContent = (newNum1 < newNum2) ? "True" : "False";
+
+  }else if (numSelector.value === "greater-than") {
+    numOutput.textContent = (newNum1 > newNum2) ? "True" : "False";
+
+  }else if (numSelector.value === "other-than") {
+    numOutput.textContent = (newNum1 !== newNum2) ? "True" : "False";
+  };
+  
+});
+
+txtButton.addEventListener("click", function() {
+  newTxt1 = txt1.value;
+  newTxt2 = txt2.value;
+  txtCompare = newTxt1.localeCompare(newTxt2);
+
+  if (txtSelector.value === "same-to") {
+    txtOutput.textContent = (txtCompare === 0) ? "True" : "False";
+
+  }else if (txtSelector.value === "different-than") {
+    txtOutput.textContent = (txtCompare === 0) ? "False" : "True";
+
+  }else if (txtSelector.value === "longer-than") {
+    txtOutput.textContent = (newTxt1.length > newTxt2.length) ? "True" : "False";
+
+  }else if (txtSelector.value === "shorter-than") {
+    txtOutput.textContent = (newTxt1.length < newTxt2.length) ? "True" : "False";
+  };
 });
