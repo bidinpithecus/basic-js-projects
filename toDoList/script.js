0ea65b2676e7fd7aa819bbe3fd88b226f19ebb16
Bidins <vinibidin@gmail.com>
2021-03-03 00:50:47 -0300

    adiciona nova feature: delete e localStorage

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,27 +1,51 @@
-const toDos = ['ITEM0', 'teste', 'vini', 'verde'];
 const input = document.getElementById('input');
 const form = document.getElementById('form');
 const lista = document.getElementById('do');
 let newElement;
+let deleteButton;
+let toDos;
+
+try {
+  toDos = JSON.parse(localStorage.getItem('toDoList'));
+} catch {
+  toDos = [];
+}
 
 function capitalize(string) {
   return string[0].toUpperCase() + string.slice(1);
 }
 
-function addToList() {
-  toDos.forEach((toDo) => {
+function renderPage() {
+  if (!toDos) {
+    toDos = [];
+  }
+  toDos.forEach((toDo, index) => {
     newElement = document.createElement('li');
-    newElement.textContent = capitalize(toDo);
+    newElement.innerHTML = `${capitalize(toDo)} `;
     lista.append(newElement);
+    deleteButton = document.createElement('button');
+    deleteButton.id = index;
+    deleteButton.addEventListener('click', (e) => {
+      lista.innerHTML = '';
+      toDos.splice(parseInt(e.target.id, 10), 1);
+      localStorage.setItem('toDoList', JSON.stringify(toDos));
+      renderPage();
+    });
+    deleteButton.textContent = 'DELETE';
+    newElement.appendChild(deleteButton);
   });
 }
-addToList();
+renderPage();
 
 function addNewToList(e) {
   e.preventDefault();
-  lista.innerText = '';
-  toDos.push(input.value);
-  addToList();
+  lista.innerHTML = '';
+  if (input.value !== '') {
+    toDos.push(input.value);
+    localStorage.setItem('toDoList', JSON.stringify(toDos));
+  }
+  renderPage();
+  input.value = '';
 }
 
 form.addEventListener('submit', addNewToList);
